9909c6cf2c6871076d2cfb753c5dff4e0cc69496
Anjan Raj <anjanraj2004@gmail.com>
2026-04-17 12:39:27 +0530

    fix: use Windows Registry for startup instead of shortcut folder

--- startup_manager.py
+++ startup_manager.py
@@ -1,61 +1,74 @@
 import sys
 import os
-import win32com.client
+import winreg
 
 class StartupManager:
     def __init__(self, app_name="Hotkey"):
         self.app_name = app_name
-        self.startup_dir = os.path.expandvars(r'%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup')
-        self.shortcut_path = os.path.join(self.startup_dir, f"{self.app_name}.lnk")
-        self.icon_path = os.path.abspath(r"assets/logo.ico")
+        self.registry_path = r"Software\Microsoft\Windows\CurrentVersion\Run"
 
     def is_startup_enabled(self):
-        return os.path.exists(self.shortcut_path)
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, self.registry_path, 0, winreg.KEY_READ)
+            value, _ = winreg.QueryValueEx(key, self.app_name)
+            winreg.CloseKey(key)
+            return True
+        except WindowsError:
+            return False
 
     def set_startup(self, enable: bool):
         if enable:
-            return self._create_shortcut()
+            return self._create_registry_key()
         else:
-            return self._remove_shortcut()
+            return self._remove_registry_key()
 
-    def _create_shortcut(self):
+    def _create_registry_key(self):
         try:
-            # Determine python executable (pythonw.exe to hide console)
             python_exe = sys.executable
             dirname = os.path.dirname(python_exe)
             pythonw = os.path.join(dirname, "pythonw.exe")
             target_exe = pythonw if os.path.exists(pythonw) else python_exe
             
             script_path = os.path.abspath("main.py")
-            working_dir = os.path.dirname(script_path)
+            command = f'"{target_exe}" "{script_path}"'
             
-            shell = win32com.client.Dispatch("WScript.Shell")
-            shortcut = shell.CreateShortCut(self.shortcut_path)
-            shortcut.TargetPath = target_exe
-            shortcut.Arguments = f'"{script_path}"'
-            shortcut.WorkingDirectory = working_dir
-            shortcut.IconLocation = self.icon_path
-            shortcut.save()
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, self.registry_path, 0, winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(key, self.app_name, 0, winreg.REG_SZ, command)
+            winreg.CloseKey(key)
+            
+            self._cleanup_old_shortcut()
+            
             return True
         except Exception as e:
-            print(f"Error creating shortcut: {e}")
+            print(f"Error creating registry key: {e}")
             return False
 
-    def _remove_shortcut(self):
+    def _remove_registry_key(self):
         try:
-            if os.path.exists(self.shortcut_path):
-                os.remove(self.shortcut_path)
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, self.registry_path, 0, winreg.KEY_SET_VALUE)
+            winreg.DeleteValue(key, self.app_name)
+            winreg.CloseKey(key)
             
-            # Also clean up old registry key if likely to exist from previous version
-            import winreg
-            try:
-                key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE)
-                winreg.DeleteValue(key, "GlobalHotkeyManager")
-                winreg.CloseKey(key)
-            except:
-                pass
-                
+            self._cleanup_old_shortcut()
+            
             return True
         except Exception as e:
-            print(f"Error removing shortcut: {e}")
+            print(f"Error removing registry key: {e}")
             return False
+
+    def _cleanup_old_shortcut(self):
+        try:
+            startup_dir = os.path.expandvars(r'%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup')
+            shortcut_path = os.path.join(startup_dir, f"{self.app_name}.lnk")
+            if os.path.exists(shortcut_path):
+                os.remove(shortcut_path)
+            
+            # Clean up old "GlobalHotkeyManager" reg key just in case
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, self.registry_path, 0, winreg.KEY_SET_VALUE)
+            try:
+                winreg.DeleteValue(key, "GlobalHotkeyManager")
+            except:
+                pass
+            winreg.CloseKey(key)
+        except Exception:
+            pass
